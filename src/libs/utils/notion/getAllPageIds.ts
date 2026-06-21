57447324bd5526e6d2dde93f57be23c0df6414dd
bd <bd@example.com>
2026-06-21 09:36:17 +0200

    fix: handle null/undefined collection_query and nested value format in getAllPageIds

--- src/libs/utils/notion/getAllPageIds.ts
+++ src/libs/utils/notion/getAllPageIds.ts
@@ -4,19 +4,19 @@ import { ExtendedRecordMap, ID } from "notion-types"
 export default function getAllPageIds(
   response: ExtendedRecordMap,
   viewId?: string
-) {
+): ID[] {
   const collectionQuery = response.collection_query
-  const views = Object.values(collectionQuery)[0]
+  const views = Object.values(collectionQuery ?? {})[0]
 
   let pageIds: ID[] = []
   if (viewId) {
     const vId = idToUuid(viewId)
-    pageIds = views[vId]?.blockIds
+    pageIds = views?.[vId]?.blockIds ?? []
   } else {
     const pageSet = new Set<ID>()
-    // * type not exist
-    Object.values(views).forEach((view: any) => {
-      view?.collection_group_results?.blockIds?.forEach((id: ID) =>
+    Object.values(views ?? {}).forEach((view: any) => {
+      const viewValue = view?.value ?? view
+      viewValue?.collection_group_results?.blockIds?.forEach((id: ID) =>
         pageSet.add(id)
       )
     })
